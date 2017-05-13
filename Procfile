6203f99db1ecc16788c33c4015d65daca9ad0f35
Anuraag Mishra <mishra.anuraag@yahoo.com>
2017-05-13 10:20:44 +0530

    added more stuff3

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java -Deserver.port=$PORT -jar build/libs/herokuReady-0.1.0.jar
+web: java $JAVA_OPTS -Dserver.port=$PORT -jar build/libs/herokuReady-0.1.0.jar
--- src/main/java/com/teamscreenbiz/api/Application.java
+++ src/main/java/com/teamscreenbiz/api/Application.java
@@ -2,27 +2,15 @@ package com.teamscreenbiz.api;
 
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
-import org.jscience.physics.model.RelativisticModel;
-import org.jscience.physics.amount.Amount;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer{
 
-
-  @RequestMapping("/")
-  @ResponseBody
-  String home() {
-    return "Hello World!";
-  }
 
 
   public static void main(String[] args) {
